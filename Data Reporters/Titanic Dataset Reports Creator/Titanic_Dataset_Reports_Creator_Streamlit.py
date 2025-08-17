3b17ba90e2cab6a23feda9a84123b8d53eb524e2
Aditya Garg <adityagarg279@gmail.com>
2025-08-17 14:33:22 +0530

    Added Altair bar graphs for Embarking Ports, Sexes, and Passenger Class to allow horizontal text x-axis lables

--- Data Reporters/Titanic Dataset Reports Creator/Titanic_Dataset_Reports_Creator_Streamlit.py
+++ Data Reporters/Titanic Dataset Reports Creator/Titanic_Dataset_Reports_Creator_Streamlit.py
@@ -23,6 +23,8 @@ in his post: https://databytesandinsights.substack.com/p/day-50-of-50-days-of-py
 import pandas as pd
 import streamlit as st
 
+import altair as alt
+
 ### List of frequently used strings
 
 #### Dataframe Column Names
@@ -40,20 +42,40 @@ METRICS_LIST = tuple((AGE, SEX, EMBARKED, PASSENGER_CLASS))
 #### Custom Columns
 FAMILY_SIZE = "FamilySize"
 
-#### Frequently used Dataframe values
-MALE = "male"
-FEMALE = "female"
 
 #### Important Statistics Strings
 SURVIVAL_RATE = "Survival Rate"
 AVERAGE_AGE = "Average Age"
 PASSENGER_CLASS_COUNTS = "Passenger Class Counts"
+EMBARKING_PORT_COUNTS = "Embarking Port Counts"
+SEX_COUNTS = "Sex Counts"
 
 #### Helper Strings
 FILTER_BY = "Filter By"
 ALL = "All"
 SELECT = "Select"
 
+
+### Dataframe categorical values and lists
+#### Port Names
+CHERBOURG = "Cherbourg"
+QUEENSTOWN = "Queenstown"
+SOUTHAMPTON = "Southampton"
+PORT_NAMES_LIST = (ALL, CHERBOURG, QUEENSTOWN, SOUTHAMPTON)
+
+#### Passenger Classes
+FIRST_CLASS = "1st Class"
+SECOND_CLASS = "2nd Class"
+THIRD_CLASS = "3rd Class"
+PASSENGER_CLASSES_LIST = (ALL, FIRST_CLASS, SECOND_CLASS, THIRD_CLASS)
+
+#### Sexes
+MALE = "male"
+FEMALE = "female"
+
+MALES = "Males"
+FEMALES = "Females"
+SEXES_LIST = (ALL, MALES, FEMALES)
 
 
 def load_titanic_data(filepath=r'TinyProjects\Data Reporters\Titanic Dataset Reports Creator\titanic.csv'):
@@ -68,14 +90,16 @@ def fillna_mode(dataseries: pd.Series):
     return dataseries.fillna(dataseries.mode()[0])
 
 
-def preprocess(df):
+def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     df[AGE] = fillna_median(df[AGE])
 
     df[EMBARKED] = fillna_mode(df[EMBARKED])
-    df[EMBARKED] = df[EMBARKED].map({'S': 0, 'C': 1, 'Q': 2})
+    df[EMBARKED] = df[EMBARKED].map({'S': "Southampton", 'C': "Cherbourg", 'Q': "Queenstown"})
     
-    df[SEX] = df[SEX].map({MALE: 0, FEMALE: 1})
+    # df[SEX] = df[SEX].map({MALE: 0, FEMALE: 1})
     
+    df[PASSENGER_CLASS] = df[PASSENGER_CLASS].map({1: "1st Class", 2: "2nd Class", 3: "3rd Class"})
+
     df[FAMILY_SIZE] = df[SIBLINGS_SPOUSE_ABOARD] + df[PARENTS_CHILDREN_ABOARD] + 1
     
     return df
@@ -84,9 +108,14 @@ def preprocess(df):
 def generate_metrics(df):
     avg_age = df[AGE].mean()
     pclass_counts = df[PASSENGER_CLASS].value_counts()
+    embarking_port_counts = df[EMBARKED].value_counts()
+    sex_counts = df[SEX].value_counts()
+
     return {
-        AVERAGE_AGE: avg_age,
-        PASSENGER_CLASS_COUNTS: pclass_counts
+        AVERAGE_AGE : avg_age,
+        PASSENGER_CLASS_COUNTS : pclass_counts,
+        EMBARKING_PORT_COUNTS : embarking_port_counts,
+        SEX_COUNTS : sex_counts
     }
 
 
@@ -95,6 +124,7 @@ def total_survival_rate(df: pd.DataFrame) -> str:
 
 def filtered_survival_rate(filteredDataFrame: pd.DataFrame, filter_string) -> str:
     return f"{SURVIVAL_RATE} for {filter_string}: {filteredDataFrame[SURVIVED].mean()*100:.2f}%"
+
 
 
 def main():
@@ -121,15 +151,86 @@ def main():
         """
         Show statistics based on Passenger Sex metric
         """
-        sex_filter = st.radio(f"{SELECT} {SEX}:", (ALL, "Males", "Females"))
+        sex_filter = st.radio(f"{SELECT} {SEX}:", SEXES_LIST)
         if sex_filter != ALL:
-            sex_val = 0 if sex_filter == "Males" else 1
+            sex_val = MALE if sex_filter == MALES else FEMALE
             filtered_df = df[df[SEX] == sex_val]
             st.write(filtered_df)
             st.write(filtered_survival_rate(filtered_df, sex_filter))
         else:
             st.write(df)
            
+        source = pd.DataFrame({
+                "Sexes" : metrics[SEX_COUNTS].index,
+                "Passenger Count by Sex (Male/Female)" : metrics[SEX_COUNTS]
+            }
+        )
+        altair_chart = alt.Chart(source).mark_bar().encode(
+            x = alt.X("Sexes", axis=alt.Axis(labelAngle=0)),
+            y = "Passenger Count by Sex (Male/Female)"
+        )
+        st.altair_chart(altair_chart)
+    
+
+    elif (metrics_selection == EMBARKED):
+        """
+        Show statistics based on Passenger Embarking Port metric
+        """
+
+        embarked_filter = st.radio(f"{SELECT} Embarking Port:", PORT_NAMES_LIST)
+
+        if embarked_filter != ALL:
+            filtered_df = df[df[EMBARKED] == embarked_filter]
+            st.write(filtered_df)
+            st.write(filtered_survival_rate(filtered_df, f"Passengers Embarking at {embarked_filter}"))
+
+        else:
+            st.write(df)
+
+        source = pd.DataFrame({
+                "Embarking Port" : metrics[EMBARKING_PORT_COUNTS].index,
+                "Embarking Passenger Counts" : metrics[EMBARKING_PORT_COUNTS]
+            }
+        )
+        altair_chart = alt.Chart(source).mark_bar().encode(
+            x = alt.X("Embarking Port", axis=alt.Axis(labelAngle=0)),
+            y = "Embarking Passenger Counts"
+        )
+        st.altair_chart(altair_chart)
+        
+
+    elif (metrics_selection == PASSENGER_CLASS):
+        """
+        Show statistics based on Passenger Class metric
+        """
+        passenger_class_filter = st.radio(f"{SELECT} {PASSENGER_CLASS}:", PASSENGER_CLASSES_LIST)
+        
+        if (passenger_class_filter != ALL):
+            # passenger_class_dict = {
+            #     "1st Class" : 1, 
+            #     "2nd Class" : 2, 
+            #     "3rd Class" : 3,
+            # }
+            filtered_df = df[df[PASSENGER_CLASS] == passenger_class_filter]
+            st.write(filtered_df)
+            st.write(filtered_survival_rate(filtered_df, passenger_class_filter))
+        else:
+            st.write(df)
+
+        # st.bar_chart(metrics[PASSENGER_CLASS_COUNTS], x=None, y=None, 
+        #              x_label="Passenger Classes", y_label="Survival Count by Passenger Class", 
+        #              color=None, horizontal=False, stack=None, width=None, height=None, use_container_width=True)
+        source = pd.DataFrame({
+                "Passenger Class" : metrics[PASSENGER_CLASS_COUNTS].index,
+                "Survival Count by Passenger Class" : metrics[PASSENGER_CLASS_COUNTS]
+            }
+        )
+        altair_chart = alt.Chart(source).mark_bar().encode(
+            x = alt.X("Passenger Class", axis=alt.Axis(labelAngle=0)),
+            y = "Survival Count by Passenger Class"
+        )
+        st.altair_chart(altair_chart)
+    
     
     elif (metrics_selection == AGE):
         """
@@ -169,47 +270,7 @@ def main():
             st.write(df)
         
     
-    elif (metrics_selection == EMBARKED):
-        """
-        Show statistics based on Passenger Embaring Port metric
-        """
-        embarked_filter = st.radio(f"{SELECT} Embarking Port:", (ALL, "Cherbourg", "Queenstown", "Southampton"))
-
-        if embarked_filter != ALL:
-            ##df[EMBARKED] = df[EMBARKED].map({'S': 0, 'C': 1, 'Q': 2})
-            embarked_port_dict = {
-                "Southampton" : 0,
-                "Cherbourg" : 1, 
-                "Queenstown" : 2, 
-            }
-            filtered_df = df[df[EMBARKED] == embarked_port_dict[embarked_filter]]
-            st.write(filtered_df)
-            st.write(filtered_survival_rate(filtered_df, embarked_filter))      
-        else:
-            st.write(df)
-
-
-    elif (metrics_selection == PASSENGER_CLASS):
-        """
-        Show statistics based on Passenger Class metric
-        """
-        passenger_class_filter = st.radio(f"{SELECT} {PASSENGER_CLASS}:", (ALL, "1st class", "2nd class", "3rd class"))
-        
-        if (passenger_class_filter != ALL):
-            passenger_class_dict = {
-                "1st class" : 1, 
-                "2nd class" : 2, 
-                "3rd class" : 3,
-            }
-            filtered_df = df[df[PASSENGER_CLASS] == passenger_class_dict[passenger_class_filter]]
-            st.write(filtered_df)
-            st.write(filtered_survival_rate(filtered_df, passenger_class_filter))
-        else:
-            st.write(df)
-
-        st.bar_chart(metrics[PASSENGER_CLASS_COUNTS], x=None, y=None, 
-                     x_label="Passenger Classes", y_label="Survival Count by Passenger Class", 
-                     color=None, horizontal=False, stack=None, width=None, height=None, use_container_width=True)
+    
         
         
 
